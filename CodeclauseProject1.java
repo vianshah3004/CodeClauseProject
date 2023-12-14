694057a0df8dfbd72352369b80a7b9db6b2aba86
Vian Shah <124880161+vianshah3004@users.noreply.github.com>
2023-12-14 13:19:57 +0530

    Add files via upload

---
+++ CodeclauseProject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.codeclauseproject1;
+
+/**
+ *
+ * @author vineet kumar shah
+ */
+public class CodeclauseProject1 {
+
+    public static void main(String[] args) {
+       NewJFrame1 jf1 = new NewJFrame1();
+       jf1.show();
+    }
+}
